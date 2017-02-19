d65aca1f89c5bd29f139288004a58587362bff0d
zhsngq <zhsngq@yeah.net>
2017-02-19 11:45:21 +0800

    edit

--- gr.sh
+++ gr.sh
@@ -1,31 +1,21 @@
 #！ /bin/bash
-echo 'tool pull ..' &&
+echo 'tool pull ...' &&
 git pull &&
-if git diff
-then
     echo 'tool add ..' &&
     git add . &&
     echo 'tool commit ..' &&
     git commit -m "edit" &&
     echo 'tool push ..' &&
     git push
-else
-    echo 'no diff file'
-fi
 echo 'hexo g ..' &&
 hexo g &&
 echo 'cd public' &&
 cd public &&
-if git diff
-then
     echo 'add ..' &&
     git add . &&
     echo 'commit ..' &&
     git commit -m "edit" &&
     echo 'push ..' &&
     git push
-else
-    echo 'no diff file'
-fi
 echo "OK"
 cd ../
