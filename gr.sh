11a318768f884f7d2991a5107e92a4d1fa00f46b
zhsngq <zhsngq@yeah.net>
2017-02-19 10:29:57 +0800

    edit

--- gr.sh
+++ gr.sh
@@ -1,10 +1,18 @@
 #！ /bin/bash
+echo 'tool add' &&
 git add . &&
+echo 'tool commit' &&
 git commit -m "edit" &&
+echo 'tool push' &&
 git push &&
+echo 'hexo' &&
 hexo g &&
+echo 'cd public' &&
 cd public &&
+echo 'add' &&
 git add . &&
+echo 'commit' &&
 git commit -m "edit" &&
+echo 'push' &&
 git push &&
 echo "OK"
